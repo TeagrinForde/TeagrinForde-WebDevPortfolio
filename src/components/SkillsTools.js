a9eb6fa883ef8ccd032fd13094c2904e933add00
Teagrin Forde <teagrinb@gmail.com>
2022-08-20 14:35:58 -0400

    current work update

--- src/components/SkillsTools.js
+++ src/components/SkillsTools.js
@@ -92,6 +92,10 @@ export default function SkillsTools() {
           <strong>Georgia Institute of Technology Full-Stack Web Development Bootcamp
           </strong>
         </h6>
+        <h6 class='center'>Applying and interviewing for positions</h6>
+        <h6 class='center'>MERN Book Search Engine</h6>
+        <h6 class='center'>React-Redux Store</h6>
+        <h6 class='center'>CK-12 Math Assessment Writer</h6>
       </div>
     </section>
   );
