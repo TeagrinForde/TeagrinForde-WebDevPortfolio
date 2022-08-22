a21dbec8d8f206b60964e6c3b3dda2841264af33
Teagrin Forde <teagrinb@gmail.com>
2022-08-22 11:05:09 -0400

    Update Skills and Tools

--- src/components/SkillsTools.js
+++ src/components/SkillsTools.js
@@ -49,11 +49,9 @@ export default function SkillsTools() {
         <div className="container" class="containerIcons">
           <div className="card" class="icon" alt='JavaScript' style={icon}>
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-            
           </div>
           <div className="card" class="icon" alt='Jest' style={icon}>
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
-            <p class="iconName">Jest</p>
           </div>
           <div className="card" class="icon" alt='jQuery' style={icon}>
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg" />
