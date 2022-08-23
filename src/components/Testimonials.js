0f1cd0821b19817996ba310a1bbb68a16fa4c5d1
Teagrin Forde <teagrinb@gmail.com>
2022-08-22 21:39:37 -0400

    project and testimony  background

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
 
 export default function Testimonials() {
-    return (
-        <section className='container'>
-        <h1 id='testimonials'>Testimonials</h1>
-        </section>
+  return (
+    <div className="container col-10 p-5" id="testimonials">
+      <div class="row center">
+        <h1 class="center p-3">Testimonials</h1>
+      </div>
 
-    );
-};
+      <div class='testimonial row center p-5'>
+        <div className='col-lg-3 col-sm-12 col-md-12 container'></div>
+
+        <div className='col-lg-6 col-sm-12 col-md-12 container'></div>
+      </div>
+
+      <div class='testimonial row center p-5'></div>
+
+      <div class='testimonial row center p-5'></div>
+
+      <div class='testimonial row center p-5'></div>
+    </div>
+  );
+}
